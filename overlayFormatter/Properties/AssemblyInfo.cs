c2ba88a4059315c40c63ad19de7ae08e6850ac00
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-03-17 01:06:36 +0000

    change app description

--- overlayFormatter/Properties/AssemblyInfo.cs
+++ overlayFormatter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("overlayFormatter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Compiles (dlc)_overlays.xml & shop_tattoo.meta into a single JSON file")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("overlayFormatter")]
